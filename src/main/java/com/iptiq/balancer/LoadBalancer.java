5a9e33a8897b8a4313adcbec7a6a3e6cc991291d
Marcos Polverini <marcos.polverini@lendex.nl>
2022-08-23 11:54:31 +0200

    added new get method

--- src/main/java/com/iptiq/balancer/LoadBalancer.java
+++ src/main/java/com/iptiq/balancer/LoadBalancer.java
@@ -1,19 +1,27 @@
 package com.iptiq.balancer;
 
 import com.iptiq.infra.CapacityExceededException;
+import com.iptiq.infra.NoProviderAvailableException;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class LoadBalancer {
 
     private final int capacity;
     private final BalancerStrategy strategy;
-    private final Set<Provider> providers = new HashSet<>();
+    private final Set<Provider> providers = ConcurrentHashMap.newKeySet();
 
     public LoadBalancer(final int capacity, final BalancerStrategy strategy) {
         this.capacity = capacity;
         this.strategy = strategy;
+    }
+
+    public String get() {
+        if (providers.isEmpty()) {
+            throw new NoProviderAvailableException();
+        }
+        return strategy.get(providers).get();
     }
 
     public boolean register(final Provider provider) {
@@ -22,7 +30,7 @@ public class LoadBalancer {
     }
 
     private void validateCapacity() throws CapacityExceededException {
-        if (providers.size() -1 == 0) {
+        if (providers.size() - 1 == 0) {
             throw new CapacityExceededException(capacity);
         }
     }
