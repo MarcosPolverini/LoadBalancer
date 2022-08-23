3387fd79298674b9629ca6ff8299e1f0add1a8c7
Marcos Polverini <marcos.polverini@lendex.nl>
2022-08-23 11:58:49 +0200

    change to constant name

--- src/main/java/com/iptiq/infra/CapacityExceededException.java
+++ src/main/java/com/iptiq/infra/CapacityExceededException.java
@@ -2,10 +2,10 @@ package com.iptiq.infra;
 
 public class CapacityExceededException extends RuntimeException {
 
-    private static final String defaultMessage = "The max capacity is %d";
+    private static final String DEFAULT_MSG = "The max capacity is %d";
 
     public CapacityExceededException(final int totalCapacity) {
-        super(String.format(defaultMessage, totalCapacity));
+        super(String.format(DEFAULT_MSG, totalCapacity));
     }
 
 }
