c15b2ae93d4a1459bcd65e268a2bbff01112d0ee
Marcos Polverini <marcos.polverini@lendex.nl>
2022-08-24 18:39:50 +0200

    Added some simulation of users getting data from the balancer

--- src/main/java/com/iptiq/Main.java
+++ src/main/java/com/iptiq/Main.java
@@ -1,9 +1,33 @@
 package com.iptiq;
 
+import com.iptiq.balancer.RandomLoadBalancerImpl;
+
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        //Simulating access
+        var providers = IntStream.range(0, 5).mapToObj(idx -> UUID.randomUUID().toString()).toArray(String[]::new);
+        try (var loadBalancer = new RandomLoadBalancerImpl(1)) {
+            loadBalancer.register(providers);
+            var numberOfInteractions = 10;
+            var executor = Executors.newFixedThreadPool(numberOfInteractions);
+            for (int i = 0; i < numberOfInteractions; i++) {
+                executor.execute(() -> {
+                    try {
+                        var waitTime = ThreadLocalRandom.current().nextLong(100, 1000);
+                        Thread.sleep(waitTime);
+                    } catch (InterruptedException e) {
+                    }
+                    System.out.println(loadBalancer.get());
+                });
+            }
+            executor.shutdown();
+        }
     }
 
 }
